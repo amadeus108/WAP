028bc6a80cb9ec4921701df44866f293ffc316aa
youu <youu@you>
2018-06-27 16:13:13 +0900

    [DB커넥션,파트너스 수정]

--- src/com/wap/service/PartnerService.java
+++ src/com/wap/service/PartnerService.java
@@ -5,12 +5,25 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+
 public class PartnerService {
 	String id;
 	String pwd;
 	String email;
-	
-	
+	String intro;
+	String pro_img;
+	String type;
+	String manager;
+	String company;
+	String ceo;
+	String sul_date;
+	String ye_name;
+	String account;
+	String last_addr;
+	int addr_id;
+	int sec_addr_id;
+	int bank;
+
 	public PartnerService()
 	{
 		
